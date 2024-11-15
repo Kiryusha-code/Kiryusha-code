f3f7d6efba3a718b3bda2df645e1c5d98ef84f80
Kiryusha-code <kiryusha.dorofeev.06@mail.ru>
2024-11-15 16:24:01 +0300

    modified:   homework 4.py

--- homework 4.py
+++ homework 4.py
@@ -1,8 +1,13 @@
-""" a = input()leng = 0
-b = 1a = a.replace('h', '!')
-for i in range(len(a) - 1):    if a[i] == a[i + 1]:
-        b += 1        leng = max(leng, b)
+a = input()
+leng = 0
+b = 1
+a = a.replace('h', '!')
+for i in range(len(a) - 1):    
+    if a[i] == a[i + 1]:
+        b += 1        
+        leng = max(leng, b)
     else:        p = 1
-print(leng, st) """
-"""a = input()b = a[a.find('(') + 1 : a.find(')')]
-print(b)"""
+print(leng, )
+a = input()
+b = a[a.find('(') + 1 : a.find(')')]
+print(b)
