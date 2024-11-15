f41429bb6cbfed686cce6107cb8dd19e494cb03f
Kiryusha-code <kiryusha.dorofeev.06@mail.ru>
2024-11-15 16:15:42 +0300

    new file:   Practicum.1.py 	new file:   homework 3.py  Untracked files: 	homework 4.py 	homework 5.py 	homework 6.py 	homework 7.py

---
+++ Practicum.1.py
@@ -0,0 +1,4 @@
+price = float(input('Ведите стоимость 1 кг конфет:'))
+for i in range(1,11):
+   print ('Стоимость' , i , 'кг. конфет', price*i )
+
---
+++ homework 3.py
@@ -0,0 +1,7 @@
+#"""a = int(input("Введите верхний предел \n"))b = 0   
+#  print(b) 
+#for i in range(1, 10):    for j in range(1,10):
+ #       print('%3d' % (i*j),end='')    print() 
+#for i in range(9,0, -1):
+ #   for j in range(1,10):        print('%3d' % (i*j),end='')
+  #  print()"""
