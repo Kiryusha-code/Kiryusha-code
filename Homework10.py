b34ac6b1d034a56148ab6d3dec48e78cd4e7626c
Kiryusha-code <kiryusha.dorofeev.06@mail.ru>
2025-05-22 11:27:42 +0300

    Update Homework10.py
    nomer 2

--- Homework10.py
+++ Homework10.py
@@ -32,3 +32,31 @@ for animal in animals:
     if isinstance(animal, MixinFly):
         print(f"- {animal.fly()}")
     print()
+# NOMER 2
+class Writer:
+    def write(self):
+        return "пишет текст"
+
+
+class Painter:
+    def draw(self):
+        return "рисует картину"
+
+
+class CreativePerson(Writer, Painter):
+    def write(self):
+        return "творчески пишет стихотворение"
+    
+    def draw(self):
+        return "выразительно рисует пейзаж"
+
+
+people = [Writer(), Painter(), CreativePerson()]
+
+for person in people:
+    print(f"{person.__class__.__name__}:")
+    if hasattr(person, 'write'):
+        print(f"- {person.write()}")
+    if hasattr(person, 'draw'):
+        print(f"- {person.draw()}")
+    print()
